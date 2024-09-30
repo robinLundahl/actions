4dc91f064bacafa34fcff24f533eba80f8d8662f
Robin Lundahl <robin.lundahl@hotmail.se>
2024-09-30 13:03:46 +0200

    navigation added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import HomeStackNavigation from "./navigation/HomeStackNavigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <HomeStackNavigation />
+    </NavigationContainer>
   );
 }
 
